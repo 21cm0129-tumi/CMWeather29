60f65b45a965b5055979276378a5356dfe0b03de
NatsumiNoda <21cm0129@jec.ac.jp>
2023-01-13 11:34:12 +0900

    Initial Commit

---
+++ CMWeather29/CMWeather29App.swift
@@ -0,0 +1,17 @@
+//
+//  CMWeather29App.swift
+//  CMWeather29
+//
+//  Created by cmStudent on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct CMWeather29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMWeather29/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CMWeather29
+//
+//  Created by cmStudent on 2023/01/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
